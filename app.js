31f8a6775aa23cc3ae1685b3fd9e97ed2c77d6b7
Mizo-Boy <dariuskorquoi@yahoo.com>
2018-05-07 19:56:01 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// var myString ="This is my string";
+
+// var myNumbers = 128847;
+
+// // console.log(myString + "" + myNumbers);
+
+
+
+// var myObj = {
+//     firstName: "Darius",
+//     lastName: "Korquoi",
+//     home: "Ganta"
+// }
+// console.log(myObj);
+
+// // if(currentIsOn) {
+// //     start the class
+// // } else {
+//     //     don't start class
+// // }
+
+// // if(birthDay) {
+// //     it is a happy time
+// // } else if(deathDay) {
+// //     it is a sad day
+// // } else { 
+// //     life must go on
+// // }
+// // var fruitArray = ["banana", "pineapple", "grapes"];
+// // var count = fruit.length;
+// // var addOne = fruitArray. push("plum")
+// // console.log(fruitArray);
+
+// // for(var i=0; i<12;i++){
+// //     console.log(i);
+// //     if(studentGrade ===100){
+// //         create a new list wirh those students
+// //     }
+// // }
+
+//  function myFunction(){
+//       for (var i=0; i<12;i++)
+//          console.log(i); 
+//  }
+
+// // console.log(myFunc)
+
+// // function addTwoNumbers(a,b){
+//  //    return a + b;
+// // }
+
+// // function multiplyTwoNumbers(g,h){
+// //     return g*h
+// // }
+// var cars = {
+//     type: "Toyota", 
+//     model: "Hpi 345", 
+//     color: "Green", 
+//     doors: "Four"
+// };
+var person = {
+    FirstName: "Darius",
+    LastName: "Korquoi",
+    age: "45", 
+    height: "5 ft.11in",
+    language: "english",
+    function(){
+        return this.FirstName +""+ this.LastName;}
+    };
+var changeText = document.get.elementById("myDiv");
+changeText.innerHTML = "this is my new text...";
